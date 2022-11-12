bf93384836b2eef12fa03625c475565637ecc773
Simon Wächter <waechter.simon@gmail.com>
2022-11-12 21:05:03 +0100

    Fix the header

--- src/libk/string.h
+++ src/libk/string.h
@@ -1,8 +1,8 @@
-#ifndef LUNARIA_OS_STRING_H
-#define LUNARIA_OS_STRING_H
+#ifndef LUNARIA_STRING_H
+#define LUNARIA_STRING_H
 
 #include <stddef.h>
 
 size_t strlen(const char* string);
 
-#endif //LUNARIA_STRING_H
+#endif // LUNARIA_STRING_H
